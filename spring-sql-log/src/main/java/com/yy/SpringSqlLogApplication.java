39d898d0376b9e50d6230c9156bc1bb4d478d1ba
yuanyang <57018014@qq.com>
2021-01-25 17:11:25 +0800

    add basic version

---
+++ spring-sql-log/src/main/java/com/yy/SpringSqlLogApplication.java
@@ -0,0 +1,15 @@
+package com.yy;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.yy.mapper")
+public class SpringSqlLogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSqlLogApplication.class, args);
+    }
+
+}
